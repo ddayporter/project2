b6e0ed944375bc106f95827c6a25ecc8934bfbae
Rachel Porter <rachelandrewsporter@gmail.com>
2015-11-19 23:22:05 -0500

    fixes juicer image and other minor style issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ User.destroy_all
 
 user = User.create(email: "rachel@email.com", password: "password")
 burgle = user.games.create(title: "Burgle Bros.", num_stars: 5, image: "burgle-bros-lineup.png", logo: "https://i.ytimg.com/vi/WFjog3wQu9k/maxresdefault.jpg", purchase_url: "http://www.burglebros.com/")
-burgle.characters.create([{name: "The Acrobat", image: "acrobat.jpg"}, {name: "The Hacker", image: "hacker.jpg"}, {name: "The Hawk", image: "hawk.jpg"}, {name: "The Juicer", image: "juicer"}, {name: "The Peterman", image: "peterman.jpg"}, {name: "The Raven", image: "raven.jpg"}, {name: "The Rigger", image: "rigger.jpg"}, {name: "The Rook", image: "rook.jpg"}, {name: "The Spotter", image: "spotter.jpg"}])
+burgle.characters.create([{name: "The Acrobat", image: "acrobat.jpg"}, {name: "The Hacker", image: "hacker.jpg"}, {name: "The Hawk", image: "hawk.jpg"}, {name: "The Juicer", image: "juicer.jpg"}, {name: "The Peterman", image: "peterman.jpg"}, {name: "The Raven", image: "raven.jpg"}, {name: "The Rigger", image: "rigger.jpg"}, {name: "The Rook", image: "rook.jpg"}, {name: "The Spotter", image: "spotter.jpg"}])
 
 # User.create([{email: 'kharvey0@netscape.com', password: 'password'}, {email: 'lhamilton1@columbia.edu', password: 'owh2841adso'}, {email: 'tkelley2@merriam-webster.com', password: 'aos1ehashoi'}, {email: 'rbryant3@163.com', password: 'aaaa111b'}, {email: 'rbell4@cnbc.com', password: 'fl1eAJoy7J'}])
 # Game.create([{title: 'Smashup', user_id: 1}, {title: 'Machi Koro', user_id: 2}, {title: 'Star Realms', user_id: 3}, {title: 'Burgle Brothers', user_id: 2}, {title: 'Star Realms', user_id: 4}, {title: 'Smashup', user_id: 2}] )
